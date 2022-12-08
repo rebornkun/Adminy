6940930b05b44bc72028b0a87b89769f8b686d37
Oluyoh okiemute <OLUYOHOKIEMUTE@GMAIL.COM>
2022-12-08 11:18:39 +0100

    fff

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { ColorModeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import TopBar from './scenes/global/TopBar'
 import './App.css'
 
 function App() {
+  const [theme , colorMode] = useMode()
 
   return (
-    <div className="app">
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className='content'>
+            <TopBar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
